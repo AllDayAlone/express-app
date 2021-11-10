d7d9e334be91f1ee4c85df5e3df2fe4d5a2a4744
Pavel Kalugin <p.kalugin@paralect.com>
2021-11-10 16:53:15 +0300

    Fix eol

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-const port = process.argv[1];
+const port = process.env.PORT;
 
 app.use((req, res) => {
   console.log(`${req.method} ${req.url}`);
